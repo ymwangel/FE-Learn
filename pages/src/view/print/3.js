9e134b64b7a0112740dfd5cb4ba3f8168d18f35b
yanmin.wang <yanmin.wang@wuage.com>
2017-07-18 18:02:01 +0800

    all

---
+++ pages/src/view/print/3.js
@@ -0,0 +1,40 @@
+(function(){
+  document.getElementById('printBtn').addEventListener('click',function(){
+      var list = document.getElementsByClassName("list")[0]
+      printElement(list)
+    })
+function printElement(div,customClass){
+      var originClassName = div.className
+      if(customClass != null){
+        div.className += ' ' + customClass
+      }else{
+        void null
+      }
+      var iframeStyle = 'display:none;'
+      var iframe = document.createElement('iframe')
+      iframe.setAttribute("style",iframeStyle)
+      document.body.appendChild(iframe)
+      var doc = iframe.contentWindow.document
+      var cssArr = document.querySelectorAll('link[rel=stylesheet]')
+      each(function(ele){
+        doc.write(ele.outerHTML)
+      },cssArr)
+      doc.write(div.outerHTML)
+      var iframeWindow = iframe.contentWindow
+
+      doc.write('<script>document.execCommand("print")</script>')
+      div.className = originClassName
+      setTimeout(function(){
+        if(iframe.remove){
+          iframe.remove()
+        }else{
+          void null
+        }
+      },1000)
+    }
+    function each(fn,xs){
+      for(var i=0;i<xs.length;i++){
+        fn(xs[i])
+      }
+    }
+})()
